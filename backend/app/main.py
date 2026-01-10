78484bb3584bb7f6870e32f88f58606b59401efa
Artyom <artyomkropp@gmail.com>
2026-01-11 01:02:37 +0900

    add app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
+from backend.interfaces.http.sessions import sessions
+
 app = FastAPI(default_response_class=ORJSONResponse)
+
+app.include_router(router=sessions.router)
